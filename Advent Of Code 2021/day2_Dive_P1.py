a0fc9400a09e95578829645af8391f15cc580fb3
Thomas Toquothty <tltoquothty@gmail.com>
2021-12-02 21:43:07 -0500

    Solved day 2, part one challenge.

--- Advent Of Code 2021/day2_Dive_P1.py
+++ Advent Of Code 2021/day2_Dive_P1.py
@@ -1,2 +1,22 @@
 # https://adventofcode.com/2021/day/2
 
+depth_position = 0
+horizontal_position = 0
+
+with open('D2_Input.txt') as input:
+    for line in input:
+        key, value = line.split()
+        if key == 'forward':
+            horizontal_position = horizontal_position + int(value)
+        #    print(horizontal_position)
+        elif key == 'down':
+            depth_position = depth_position + int(value)
+        else:
+            depth_position = depth_position - int(value)
+
+print(f"The final depth_position is {depth_position}")
+print(f"The final horizontal position is {horizontal_position}")
+
+final_position = depth_position * horizontal_position
+
+print(f"The final position is {final_position}")
