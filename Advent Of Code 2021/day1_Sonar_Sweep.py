f2fbacf30649904639420ab79b1d0aabb156af67
Thomas Toquothty <tltoquothty@gmail.com>
2021-12-02 20:46:10 -0500

    First day of advent code challenge 1.

---
+++ Advent Of Code 2021/day1_Sonar_Sweep.py
@@ -0,0 +1,19 @@
+# https://adventofcode.com/2021/day/1
+
+# Count the number of times a depth measurement increases from the previous measurement.
+
+
+increased_lines = 0
+new_line = 0
+with open('D1_Input.txt') as input:
+    for line in input:
+        if line > new_line:
+            increased_lines += 1
+            new_line = line
+            print(f"{new_line} (increased)")
+        else:
+            new_line = line
+            print(f"{new_line} (decreased)")
+
+
+
