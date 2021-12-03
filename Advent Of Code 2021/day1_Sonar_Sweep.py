84e43f61e8e6de87fcd0dd1b6fe3cb91d88dcd72
Thomas Toquothty <tltoquothty@gmail.com>
2021-12-02 20:50:51 -0500

    Day one, challenge one successful.

--- Advent Of Code 2021/day1_Sonar_Sweep.py
+++ Advent Of Code 2021/day1_Sonar_Sweep.py
@@ -7,6 +7,7 @@ increased_lines = 0
 new_line = 0
 with open('D1_Input.txt') as input:
     for line in input:
+        line = int(line)
         if line > new_line:
             increased_lines += 1
             new_line = line
@@ -15,5 +16,7 @@ with open('D1_Input.txt') as input:
             new_line = line
             print(f"{new_line} (decreased)")
 
+increased_lines = increased_lines - 1
+print(f"The total increased lines are: {increased_lines}")
 
 
