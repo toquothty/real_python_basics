a6827d1883a3fd524f24dd7396c9e3cd1aa6fec0
Thomas Toquothty <tltoquothty@gmail.com>
2021-12-08 15:50:15 -0500

    AOC work

--- Advent Of Code 2021/day3_Binary_Diagnostic.py
+++ Advent Of Code 2021/day3_Binary_Diagnostic.py
@@ -1,5 +1,14 @@
 # https://adventofcode.com/2021/day/3
 
-gamma_rate = 0
-epsilon_rate = 0
-power_consumption = gamma_rate * epsilon_rate
+gamma_rate = []
+epsilon_rate = []
+# power_consumption = gamma_rate * epsilon_rate
+
+#dictionary = ['A','B','C','D','E','F','G','H','I','J','K','L']
+
+with open('D3_Input.txt') as input:
+    for line in input:
+        line_list = list(line)
+        for n in range(0, len(line_list) - 1):
+            for i in line_list[n]:
+                print(i)
