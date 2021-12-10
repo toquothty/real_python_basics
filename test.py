b63f908fe3bcc0ff146092bdb1a11dae83d9e5ce
Thomas Toquothty <tltoquothty@gmail.com>
2021-12-10 16:41:34 -0500

    Chapter 12.5 review exercises

--- test.py
+++ test.py
@@ -3,12 +3,25 @@
 
 from pathlib import Path
 
-lines_of_text = ["Hello on line 1\n", "Hello on line 2\n", "wiggida\n"]
+lines_of_text = ["Discovery\n", "Enterprise\n", "Defiant\n", "Voyager\n"]
 
-path = Path.home() / "hello.txt"
-with path.open(mode="a", encoding="utf-8") as file:
+# path = Path.home() / "hello.txt"
+# with path.open(mode="a", encoding="utf-8") as file:
+#     file.writelines(lines_of_text)
+
+# with path.open(mode="r", encoding="utf-8") as file:
+#     for line in file.readlines():
+#         print(line, end="")
+
+path = Path.home() / "starships.txt"
+with path.open(mode="w", encoding="utf-8") as file:
     file.writelines(lines_of_text)
 
 with path.open(mode="r", encoding="utf-8") as file:
     for line in file.readlines():
         print(line, end="")
+
+with path.open(mode="r", encoding="utf-8") as file:
+    for line in file.readlines():
+        if line[0] == "D":
+            print(line, end="")
