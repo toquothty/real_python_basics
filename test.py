4491cdc6fb052991cf4bb0ad6d5bb27388b01b67
Thomas Toquothty <tltoquothty@gmail.com>
2021-12-10 16:33:32 -0500

    Chapter 12.5 follow along.

--- test.py
+++ test.py
@@ -2,31 +2,13 @@
 #
 
 from pathlib import Path
-import shutil
 
-my_folder = Path.home() / "my_folder"
-my_folder.mkdir(exist_ok=True)
+lines_of_text = ["Hello on line 1\n", "Hello on line 2\n", "wiggida\n"]
 
+path = Path.home() / "hello.txt"
+with path.open(mode="a", encoding="utf-8") as file:
+    file.writelines(lines_of_text)
 
-paths = [
-    my_folder / "file1.txt",
-    my_folder / "file2.txt",
-    my_folder / "image1.png"
-]
-
-for path in paths:
-    path.touch()
-
-source = my_folder / "image1.png"
-destination = my_folder / "images" / "image1.png"
-
-images = Path.home() / "my_folder" / "images"
-images.mkdir(exist_ok=True)
-
-source.replace(destination)
-
-del_file = my_folder / "file1.txt"
-del_file.unlink()
-
-del_structure = my_folder
-shutil.rmtree(del_structure)
+with path.open(mode="r", encoding="utf-8") as file:
+    for line in file.readlines():
+        print(line, end="")
