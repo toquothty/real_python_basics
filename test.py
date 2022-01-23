0a1371f0c7dd58d132cca12f8a7f7c140ec5899d
Thomas Toquothty <tltoquothty@gmail.com>
2022-01-22 21:01:44 -0500

    Follow along practice from chapter 14

--- test.py
+++ test.py
@@ -1,59 +1,26 @@
 # This space is just for chapter follow along and review exercises.
 #
 
+from PyPDF2 import PdfFileReader
 from pathlib import Path
-import csv
 
-# Review Exercise 1
-# file_path = Path.home() / "numbers.csv"
+pdf_path = (
+    Path.home()
+    / "Python"
+    / "python-basics-exercises-master"
+    / "ch14-interact-with-pdf-files"
+    / "practice_files"
+    / "Pride_and_Prejudice.pdf"
+)
 
-# numbers = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]]
+pdf_reader = PdfFileReader(str(pdf_path))
+output_file_path = Path.home() / "Pride_and_Prejudice.txt"
 
-# with file_path.open(mode="w", encoding="utf-8", newline="") as file:
-#     writer = csv.writer(file)
-#     writer.writerows(numbers)
+with output_file_path.open(mode="w") as output_file:
+    title = pdf_reader.documentInfo.title
+    num_pages = pdf_reader.getNumPages()
+    output_file.write(f"{title}\nNumber of pages: {num_pages}\n\n")
 
-# ////////////////////////////////////////////////////////////////////////////
-
-# Review Exercise 2
-# file_path = Path.home() / "numbers.csv"
-
-# numbers = []
-
-# with file_path.open(mode="r", encoding="utf-8", newline="") as file:
-#     reader = csv.reader(file)
-#     for row in reader:
-#         int_row = [int(value) for value in row]
-#         numbers.append(int_row)
-# print(numbers)
-
-# //////////////////////////////////////////////////////////////////////////
-
-# Review Exercise 3
-
-# file_path = Path.home() / "favorite_colors.csv"
-
-# favorite_colors = [
-#     {"name": "Joe", "favorite_color": "blue"},
-#     {"name": "Anne", "favorite_color": "green"},
-#     {"name": "Bailey", "favorite_color": "red"},
-# ]
-
-# with file_path.open(mode="w", encoding="utf-8", newline="") as file:
-#     writer = csv.DictWriter(file, fieldnames=favorite_colors[0].keys())
-#     writer.writeheader()
-#     writer.writerows(favorite_colors)
-
-# /////////////////////////////////////////////////////////////////////////
-
-# Review Exercise 4
-
-file_path = Path.home() / "favorite_colors.csv"
-
-favorite_colors = []
-
-with file_path.open(mode="r", encoding="utf-8", newline="") as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        favorite_colors.append(row)
-print(favorite_colors)
+    for page in pdf_reader.pages:
+        text = page.extractText()
+        output_file.write(text)
