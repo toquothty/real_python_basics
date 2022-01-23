b31b2d463c623396b1421ffe6719543e8c147793
Thomas Toquothty <tltoquothty@gmail.com>
2022-01-22 21:06:12 -0500

    Knocked out section 1 review exercises

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 # This space is just for chapter follow along and review exercises.
 #
 
+from traceback import print_tb
 from PyPDF2 import PdfFileReader
 from pathlib import Path
 
@@ -10,17 +11,20 @@ pdf_path = (
     / "python-basics-exercises-master"
     / "ch14-interact-with-pdf-files"
     / "practice_files"
-    / "Pride_and_Prejudice.pdf"
+    / "zen.pdf"
 )
 
 pdf_reader = PdfFileReader(str(pdf_path))
-output_file_path = Path.home() / "Pride_and_Prejudice.txt"
+print(pdf_reader.getNumPages())
+first_page = pdf_reader.getPage(0)
+print(first_page.extractText())
+# output_file_path = Path.home() / "Pride_and_Prejudice.txt"
 
-with output_file_path.open(mode="w") as output_file:
-    title = pdf_reader.documentInfo.title
-    num_pages = pdf_reader.getNumPages()
-    output_file.write(f"{title}\nNumber of pages: {num_pages}\n\n")
+# with output_file_path.open(mode="w") as output_file:
+#     title = pdf_reader.documentInfo.title
+#     num_pages = pdf_reader.getNumPages()
+#     output_file.write(f"{title}\nNumber of pages: {num_pages}\n\n")
 
-    for page in pdf_reader.pages:
-        text = page.extractText()
-        output_file.write(text)
+#     for page in pdf_reader.pages:
+#         text = page.extractText()
+#         output_file.write(text)
