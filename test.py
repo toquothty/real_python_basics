ec3a96755dbbf0acd693e8af7e60bbcdaa14eca0
Thomas Toquothty <tltoquothty@gmail.com>
2022-01-26 16:05:52 -0500

    Follow along practice C15

--- test.py
+++ test.py
@@ -1,30 +1,15 @@
 # This space is just for chapter follow along and review exercises.
 #
 
-from traceback import print_tb
-from PyPDF2 import PdfFileReader
-from pathlib import Path
+import sqlite3
 
-pdf_path = (
-    Path.home()
-    / "Python"
-    / "python-basics-exercises-master"
-    / "ch14-interact-with-pdf-files"
-    / "practice_files"
-    / "zen.pdf"
-)
+connection = sqlite3.connect("test_database.db")
+cursor = connection.cursor()
 
-pdf_reader = PdfFileReader(str(pdf_path))
-print(pdf_reader.getNumPages())
-first_page = pdf_reader.getPage(0)
-print(first_page.extractText())
-# output_file_path = Path.home() / "Pride_and_Prejudice.txt"
+query = "SELECT datetime('now', 'localtime');"
+results = cursor.execute(query)
 
-# with output_file_path.open(mode="w") as output_file:
-#     title = pdf_reader.documentInfo.title
-#     num_pages = pdf_reader.getNumPages()
-#     output_file.write(f"{title}\nNumber of pages: {num_pages}\n\n")
+row = results.fetchone()
+print(row)
 
-#     for page in pdf_reader.pages:
-#         text = page.extractText()
-#         output_file.write(text)
+connection.close()
