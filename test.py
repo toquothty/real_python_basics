22249a333e20bfa23e357bf0ba51f32ab6df2a4c
Thomas Toquothty <tltoquothty@gmail.com>
2022-01-26 16:09:47 -0500

    follow along work

--- test.py
+++ test.py
@@ -3,13 +3,10 @@
 
 import sqlite3
 
-connection = sqlite3.connect("test_database.db")
-cursor = connection.cursor()
-
-query = "SELECT datetime('now', 'localtime');"
-results = cursor.execute(query)
-
-row = results.fetchone()
-print(row)
-
-connection.close()
+with sqlite3.connect("test_database.db") as connection:
+    cursor = connection.cursor()
+    query = "SELECT datetime('now', 'localtime');"
+    results = cursor.execute(query)
+    row = results.fetchone()
+    time = row[0]
+    print(time)
