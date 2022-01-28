6c71e00399315bae2affbd7f7bce644d2c3e893b
Thomas Toquothty <tltoquothty@gmail.com>
2022-01-28 16:12:32 -0500

    Follow along CH16

--- test.py
+++ test.py
@@ -1,19 +1,18 @@
 # This space is just for chapter follow along and review exercises.
 #
 
-from turtle import title
+import re
 from urllib.request import urlopen
 
-url = "http://olympus.realpython.org/profiles/aphrodite"
+url = "http://olympus.realpython.org/profiles/dionysus"
 page = urlopen(url)
 
 html_bytes = page.read()
 html = html_bytes.decode("utf-8")
 
-title_index = html.find("<title>")
-start_index = title_index + len("<title>")
-end_index = html.find("</title>")
-
-title = html[start_index:end_index]
+pattern = "<title.*?>.*?</title.*?>"
+match_results = re.search(pattern, html, re.IGNORECASE)
+title = match_results.group()
+title = re.sub("<.*?>", "", title)  # Remove HTML tags
 
 print(title)
