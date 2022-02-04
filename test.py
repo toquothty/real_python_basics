c4d06123094f390e15998f45b758cb196c8fe2a4
Thomas Toquothty <tltoquothty@gmail.com>
2022-02-04 14:45:54 -0500

    Review exercise attempt CH 16.3

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 # This space is just for chapter follow along and review exercises.
 #
 
-from distutils.log import log
+
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import mechanicalsoup
@@ -11,15 +11,26 @@ url = "http://olympus.realpython.org/login"
 login_page = browser.get(url)
 login_html = login_page.soup
 
+# Exercise 1
 form = login_html.select("form")[0]
 form.select("input")[0]["value"] = "zeus"
 form.select("input")[1]["value"] = "ThunderDude"
 
 profiles_page = browser.submit(form, login_page.url)
 
-links = profiles_page.soup.select("a")
+# Exercise 2
+print(profiles_page.soup.title.string)
 
-for link in links:
-    address = link["href"]
-    text = link.text
-    print(f"{text}: {address}")
+# Exercise 3
+url = "http://olympus.realpython.org/login"
+login_page = browser.get(url)
+login_html = login_page.soup
+
+
+# Exercise 4
+form = login_html.select("form")[0]
+form.select("input")[0]["value"] = "zeus"
+form.select("input")[1]["value"] = "ThunderDde"
+
+profiles_page = browser.submit(form, login_page.url)
+print(profiles_page.soup.center.text)
